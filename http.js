f35774678814df5ceac5db3ead18eeee3a2c7517
Lijing Wang <lijing@ualberta.ca>
2018-01-23 00:51:02 +0000

    add function requesting and log the response data

---
+++ http.js
@@ -0,0 +1,20 @@
+var https = require('https');
+
+
+
+//every chunks strats in a new line. not a well-writen document
+var getAndPrintHTMLChunks = function(){
+  var requireOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requireOptions, function(response){
+    response.setEncoding('utf8');
+    response.on('data', function(data){
+      console.log('Chunk Received: ' + data + '\n');
+    });
+  });
+};
+
+getAndPrintHTMLChunks();
